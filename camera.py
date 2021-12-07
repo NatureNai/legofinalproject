b8afa1537e030d11f91210e39296dd674a1407be
Nailah Brogdon <93626966+NatureNai@users.noreply.github.com>
2021-12-06 19:30:14 -0500

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,3 +1,4 @@
+#ensuring that the camera works properly
 from picamera import PiCamera
 from time import sleep
 
@@ -6,3 +7,5 @@ camera = PiCamera()
 camera.start_preview()
 sleep(5)
 camera.stop_preview()
+
+#https://projects.raspberrypi.org/en/projects/getting-started-with-picamera/4
